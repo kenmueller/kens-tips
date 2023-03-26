16c0d3597f5d5208371a2a72f125618841eefd28
Ken Mueller <kenmueller0@gmail.com>
2023-03-26 15:20:32 -0700

    robots.txt

---
+++ app/robots.txt/route.ts
@@ -0,0 +1,32 @@
+if (!process.env.NEXT_PUBLIC_ORIGIN)
+	throw new Error('Missing NEXT_PUBLIC_ORIGIN')
+
+import { NextResponse } from 'next/server'
+
+import errorFromUnknown from '@/lib/error/fromUnknown'
+import getQuestionNames from '@/lib/question/getNames'
+
+const getPaths = async () => [
+	'',
+	...(await getQuestionNames()).map(
+		question => `/q/${encodeURIComponent(question)}`
+	)
+]
+
+export const GET = async () => {
+	try {
+		return new NextResponse(
+			`User-agent: *
+Sitemap: ${process.env.NEXT_PUBLIC_ORIGIN!}/sitemap.xml`,
+			{
+				headers: {
+					'cache-control': 'no-cache',
+					'content-type': 'text/plain'
+				}
+			}
+		)
+	} catch (unknownError) {
+		const { code, message } = errorFromUnknown(unknownError)
+		return new NextResponse(message, { status: code })
+	}
+}
